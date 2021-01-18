6c4333743a2c40e47938639bfbfe67a4b1dee531
nawla02 <73086433+nawla02@users.noreply.github.com>
2021-01-18 20:47:34 +0700

    Add files via upload

---
+++ MainSeg.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainseg;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class MainSeg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Main Luas SEgitiga 
+           MainSeg hai = new (); 
+           
+        
+        
+        
+    }
+    
+}
